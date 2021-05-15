61c8c180ca567211b6011571ee1dfc325878f8b6
shadowjljp <jerryj1205@gmail.com>
2021-05-15 16:20:26 -0500

    backend establish

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express"
+import cors from "cors"
+import restaurants from "./api/restaurants.route.js"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())  // allow server to accept json
+app.use("/api/v1/restaurants",restaurants)
+app.use("*",(req,res) => res.status(404).json({error:"not found "})) 
